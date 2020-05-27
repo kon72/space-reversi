cfb53954ee9151faafaa7a573fb014cf1d5b7457
unknown <kinsei0916@gmail.com>
2020-05-27 17:56:45 +0900

    Updates

---
+++ precache-manifest.d3e714f3e6f2c32f5e72ffe4a40d41fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798392c09bedcc5ee27fb8e3dea1556d",
+    "url": "/space-reversi/index.html"
+  },
+  {
+    "revision": "f7cbdcd204d689737c7c",
+    "url": "/space-reversi/static/css/main.6847f0fd.chunk.css"
+  },
+  {
+    "revision": "b4716500f0e62820d46c",
+    "url": "/space-reversi/static/js/2.0fd37ac8.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/space-reversi/static/js/2.0fd37ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7cbdcd204d689737c7c",
+    "url": "/space-reversi/static/js/main.59865fb8.chunk.js"
+  },
+  {
+    "revision": "daef576dfbe915384f16",
+    "url": "/space-reversi/static/js/runtime-main.57538f91.js"
+  }
+]);
